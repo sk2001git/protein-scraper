0f5ba1701f5b52a6b30daf4a16766c237e85e9ee
sk2001git <122198303+sk2001git@users.noreply.github.com>
2024-06-08 00:16:25 +0800

    Add edge to pages

--- pages/api/scrape.ts
+++ pages/api/scrape.ts
@@ -1,6 +1,8 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { firecrawl_scrape } from '@/backend/scraping';
 
+export const runtime = 'edge';
+
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'POST') {
     const { url } = req.body;
