e0f61cd7ac56df3feb39c4e0a5e37dea830c75f7
sk2001git <122198303+sk2001git@users.noreply.github.com>
2024-07-13 19:07:39 +0800

    Fix runtime edge

--- pages/api/test/pupeeter.ts
+++ pages/api/test/pupeeter.ts
@@ -1,5 +1,7 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
+export const runtime = 'edge';
+
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
     res.status(200).json({
